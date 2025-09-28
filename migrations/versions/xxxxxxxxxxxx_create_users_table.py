2870d9bc5ddc2d97d507198695db1c20ebdf4bbf
colmeta <nsubugacollin@gmail.com>
2025-09-28 08:39:11 +0300

    Update xxxxxxxxxxxx_create_users_table.py

--- migrations/versions/xxxxxxxxxxxx_create_users_table.py
+++ migrations/versions/xxxxxxxxxxxx_create_users_table.py
@@ -9,7 +9,7 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision = 'some_unique_id'
+revision = '0001_initial_schema'
 down_revision = None
 branch_labels = None
 depends_on = None
